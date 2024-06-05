34e602487258d2df705ab568101232740ed370c0
Sean <seanhyu@gmail.com>
2024-06-06 06:25:48 +0800

    Northern Lights and Solar Eclipse

--- src/pages/locations/NorthernLights.js
+++ src/pages/locations/NorthernLights.js
@@ -3,11 +3,33 @@ import Standard from '../../components/standard/Standard'
 import "./LocationsStyles.css"
 
 import CanadaLights from '../../assets/northernLights.jpg'
+import IcelandLights from '../../assets/northernLightsIceland.jpg'
 
 export function NorthernLights () {
     return (
         <div>
-            <Standard bgImg={CanadaLights} text="Northern Lights" />
+            <Standard bgImg={IcelandLights} text="Northern Lights" />
+            <div className='container'>
+                <div className='all-text'>
+                    <p>The Northern Lights are one of the most magical phenomenon I've ever seen. But they can also be quite elusive. While a degree of luck is definitely needed, there are definitely ways to maximize your chances of seeing the northern lights.</p>
+                </div>
+                <h3>Finding the Northern Lights</h3>
+                <div className='image-text'>
+                    <img src={CanadaLights} alt="Northern Lights in Canada"/>
+                    <p>
+                        There are four main factors to consider when planning out a Northern Lights trip. 
+                        <br/><br/>The first is location. You want to travel to a place as close to the aurora oval as possible. The aurora oval is a ring-shaped region around the magnetic poles where the aurora borealis tends to be the strongest. Generally this means getting yourself close to the artic circle. Some of the best places to visit are Fairbanks in Alaska, Tromso in Norway, and Iceland. 
+                        Once you are there, you will need to get yourself as far away from civilization as possible to avoid light pollution.
+                        <br/><br/>The second factor is the sky conditions. Is the sky cloudy? If so, try to move around to find a place where it isn't. Places like Iceland tend to have very hit or miss weather, so if northern lights is all you are going after, I would recommend Fairbanks or Tromso over Iceland.
+                        <br/><br/>Third, you must consider timing. During the summer, the northern lights are not visible throughout most of the arctic circle because the sky never turns dark during these months. So you will need to choose a date generally between late August and April depending on where you are. The fall and spring equinoxes also have historically been when aurora activity has been greatest so try timing your visit around these dates. Furthermore, visiting during the Solar Maximum (the period within the 11 year solar cycle where the sun is most unstable) is preferable! 
+                        You should also try to visit during a new moon to eliminate light pollution caused by the moon.
+                        <br/><br/>Finally, you need to consider the Kp. The Kp index is a scale from 1-9 that essentially measures the strength of the aurora. Depending on your location, a Kp index of 2 or higher is likely needed for you to see the northern lights, and often times only when the index is above 4 are the lights vivid enough for the colors to really pop to the naked eye. I recommend getting the Hello Aurora app, which tells you what the current aurora strengths are and also alerts you when others nearby have reported seeing the northern lights.
+                    </p>
+                </div>
+                <div className='all-text'>
+                    <p>In the end, you will need luck to be on your side to witness the northern lights. So I recommend staying at least one week at any location to maximize your chances of catching the lights. I wish everyone the best of luck in your northern lights hunting excursions!</p>
+                </div>
+            </div>
         </div>
 
     )
--- src/pages/locations/SolarEclipse.js
+++ src/pages/locations/SolarEclipse.js
@@ -3,11 +3,34 @@ import Standard from '../../components/standard/Standard'
 import "./LocationsStyles.css"
 
 import CanadaEclipse from '../../assets/solarEclipse.jpg'
+import Beads from '../../assets/beads.jpg'
+import DiamondRing from '../../assets/diamondRing.jpg'
 
 export function SolarEclipse () {
     return (
         <div>
-            <Standard bgImg={CanadaEclipse} text="Solar Eclipses" />
+            <Standard bgImg={Beads} text="Solar Eclipses" />
+            <div className='container'>
+                <div className='all-text'>
+                    <p>Witnessing a total solar eclipse is truly a once-in-a-lifetime experience. To see the sun blocked by the moon, with its corona shining it, is such a surreal and magical moment. If you ever have the chance to witness a total solar eclipse, you should absolutely go out of your way to make it to the path of totality.</p>
+                </div>
+                <h3>Total vs Partial Solar Eclipse</h3>
+                <div className='image-text'>
+                    <img src={CanadaEclipse} alt="Bailey's Beads"/>
+                    <p>
+                        In April of 2023, many of us in the U.S. had the chance to witness a solar eclipse. However, the vast majority of people only got to see a partial solar eclipse, where the sun is only partially blocked by the moon.
+                        <br/><br/>While seeing a partial eclipse is still cool, it simply cannot compare to the experience of being in the path of totality and seeing the sun fully blocked. During partial eclipses, one must wear solar eclipse glasses for the entire duration of the eclipse. 
+                        <br/><br/>However if you are located in the path of totality, you can actually take off your glasses and look directly at the sun during the few minutes of totality. The sky rapidly darkens in the few minutes before totality as the sun gets covered, and when totality occurs it is almost as dark as night and you can see the stars.
+                        <br/><br/>If you didn't get the chance to witness totality during the April 2023 Solar Eclipse, I highly recommend travelling out to Europe in 2026 for the total solar eclipse that is happening there! The path of totality crosses portions of Iceland, Spain, and Portugal. Just be sure to check the weather beforehand so that you can position yourself where it won't be cloudy during the eclipse. 
+                        <br/><br/>
+                    </p>
+                </div>
+                <h3>Photographing Totality</h3>
+                <div className='all-text'>
+                    <p>If you are interested in photographing the moment of totality, you will need at least a 200mm lens, and preferably even higher. My photos here were taken with a 200mm lens, and while they do show an accurate depiction of what totality looks like, they are relatively lower quality because I had to crop into the image. 
+                        <br/><br/>Be sure to set your camera's focus manually since auto-focus will not be able to properly focus on the moon and Sun. I also recommend using bracket-mode to capture a large range of exposures since it can be difficult to expose the corona properly with a single shot. This also removes a degree of human error, which is important since totality only lasts a few minutes and you need to save time to actually enjoy the eclipse with your own eyes!</p>
+                </div>
+            </div>
         </div>
 
     )
